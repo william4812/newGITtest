3cf3b0a3c06f6fee9bf5815d67e1cc6d2d1bb43a
William <williams19834812@gmail.com>
2020-04-22 16:28:32 -0400

    more specific

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,6 @@
 print("Hello world")
 print("Change on X1 carbon extreme 2nd gen.")
 
-print("Something change on X1 carbon 2013")
+print("Something alternative on X1 carbon 2013 - more and more")
+
+
