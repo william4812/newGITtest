41a6223f793066d3bc8c1c33f783164aecb29e71
William <williams19834812@gmail.com>
2020-04-23 11:35:44 -0400

    vim tested

--- HelloWorld.py
+++ HelloWorld.py
@@ -2,3 +2,6 @@ print("Hello world")
 print("Change on X1 carbon extreme 2nd gen.")
 
 print("Something alternative, change difference on X1 carbon 2013 - less and more or more and more")
+
+print("Vim tested here")
+
